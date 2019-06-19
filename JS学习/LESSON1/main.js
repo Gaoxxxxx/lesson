48f5171a3906a2627ab912e3c79c363f189ce387
Gaoxxxxx <13100818206@163.com>
2019-06-19 22:26:51 +0800

    JavaScript学习指南第一章

--- JS学习/LESSON1/main.js
+++ JS学习/LESSON1/main.js
@@ -1,5 +1,5 @@
 //所有HTML文件加载完成之后才执行js文件
 $(document).ready(function(){
-    'use strict';
-    console.log('main.jsloaded');
+    'use strict'; 
+    document.getElementById("mainCanvas").innerHTML = "Hello JavaScript";
 })
