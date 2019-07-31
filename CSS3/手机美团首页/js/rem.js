4ea1f87bb380cbf1d1788a197a41c96145e1be95
Gaoxxxxx <13100818206@163.com>
2019-07-31 11:14:39 +0800

    美团首页

---
+++ CSS3/手机美团首页/js/rem.js
@@ -0,0 +1,33 @@
+;
+(function(win) {
+    var doc = win.document;
+    var docEl = doc.documentElement;
+    var tid;
+
+    function refreshRem() {
+        // 获取屏幕宽度
+        var width = docEl.getBoundingClientRect().width;
+        var rem = width / 7.5; // 将屏幕宽度分成7.5份，
+        //750/7.5     html的fontsize  = 100
+        // 375/7.5    html的fontsize  =  50
+        // 1份为1rem
+
+        docEl.style.fontSize = rem + 'px';
+        console.log(0.14*rem);
+    }
+
+    win.addEventListener('resize', function() {
+        clearTimeout(tid);
+        tid = setTimeout(refreshRem, 10);
+    }, false);
+    win.addEventListener('pageshow', function(e) {
+        if (e.persisted) {
+            clearTimeout(tid);
+            tid = setTimeout(refreshRem, 10);
+        }
+    }, false);
+
+    refreshRem();
+
+})(window);
+
